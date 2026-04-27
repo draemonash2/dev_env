9dddbddf9935a8e9c04e800ec445aff32fabd81d
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2026-04-28 05:57:04 +0900

    update attach commands

--- attach.sh
+++ attach.sh
@@ -2,5 +2,4 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "${SCRIPT_DIR}/_env_setting.sh"
 docker start $CONTAINER_NAME
-# docker attach $CONTAINER_NAME
-docker exec -it $CONTAINER_NAME /bin/bash
+docker exec -e DISPLAY -it $CONTAINER_NAME /ros_entrypoint.sh bash
